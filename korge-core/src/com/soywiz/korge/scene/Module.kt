2a79292c1f898981daa2c9b785c42df09e5eda9a
soywiz <soywiz@gmail.com>
2017-09-09 20:52:49 +0200

    Small fix

--- korge-core/src/com/soywiz/korge/scene/Module.kt
+++ korge-core/src/com/soywiz/korge/scene/Module.kt
@@ -8,8 +8,8 @@ open class Module {
 	open val bgcolor: Int = Colors.BLACK
 	open val title: String = "Game"
 	open val icon: String? = null
-	open val size: SizeInt = SizeInt(640, 480)
-	open val windowSize: SizeInt = size
+	open val size: SizeInt by lazy { SizeInt(640, 480) }
+	open val windowSize: SizeInt get() = size
 
 	open val mainScene: Class<out Scene> = EmptyScene::class.java
 	open val clearEachFrame = true
